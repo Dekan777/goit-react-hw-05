4c16352c2eb04256e7a2067245acbd7d3d9aa3bf
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-02-02 16:11:17 +0100

    name ''

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,16 +1,41 @@
 import css from './SearchBox.module.css';
-import { HiSearch } from 'react-icons/hi';
+import { useRef } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
 
-export const SearchBox = ({ value, onChange }) => {
+export const SearchBox = ({ initialValue, onSearch }) => {
+  const inputRef = useRef();
+
+  const handleSubmit = evt => {
+    evt.preventDefault();
+    const inputValue = inputRef.current.value;
+
+    if (!inputValue.trim()) {
+      toast.error('Please enter a value');
+      return;
+    }
+
+    onSearch(inputValue);
+    evt.target.reset(); // Reset the form directly
+  };
   return (
-    <div className={css.wrapper}>
-      <HiSearch className={css.icon} />
-      <input
-        className={css.input}
-        type="text"
-        value={value}
-        onChange={e => onChange(e.target.value)}
-      />
+    <div className={css.container}>
+      <form onSubmit={handleSubmit}>
+        <div className={css.inputContainer}>
+          <input
+            ref={inputRef}
+            type="text"
+            autoComplete="off"
+            autoFocus={true}
+            placeholder=""
+            className={css.inputField}
+            defaultValue={initialValue} // Use defaultValue instead of value
+          />
+          <button type="submit" className={css.submitButton}>
+            Search
+          </button>
+        </div>
+        <Toaster containerStyle={{ marginTop: 85 }} />
+      </form>
     </div>
   );
 };
--- src/pages/MoviePage/MoviesPage.jsx
+++ src/pages/MoviePage/MoviesPage.jsx
@@ -1,10 +1,22 @@
-// import { useSearchParams } from 'react-router-dom';
+import { useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import { SearchBox } from '../../components/SearchBox/SearchBox';
 
 export const MoviesPage = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [productName, setProductName] = useState(
+    searchParams.get('name') ?? ''
+  );
+
+  const handleSearch = name => {
+    const nextParams = name !== '' ? { name } : {};
+    setSearchParams(nextParams);
+    setProductName(''); // Обновляем локальное состояние для очистки формы
+  };
+
   return (
     <main>
-      <SearchBox />
+      <SearchBox initialValue={productName} onSearch={handleSearch} />
     </main>
   );
 };
