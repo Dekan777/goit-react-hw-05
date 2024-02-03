c9ba1758ab31b52e1fdcfe93bfacdaa854b242b5
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-02-03 21:03:13 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom'; // Изменение импорта на HashRouter
+import { BrowserRouter } from 'react-router-dom';
 import 'modern-normalize';
 import { App } from './components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
-      {' '}
-      {/* Замена BrowserRouter на HashRouter */}
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { HashRouter } from 'react-router-dom'; // Изменение импорта на HashRouter
+// import 'modern-normalize';
+// import { App } from './components/App';
+// import './index.css';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <HashRouter>
+//       {' '}
+//       {/* Замена BrowserRouter на HashRouter */}
+//       <App />
+//     </HashRouter>
+//   </React.StrictMode>
+// );
